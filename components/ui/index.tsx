d376c57c0bb5c899acca5b14712e00a8230c883e
Hemant Rajpurohit <129470020+Hemant-exe@users.noreply.github.com>
2025-02-02 22:50:43 +0530

    Delete components directory

--- components/ui/index.tsx
+++
@@ -1,6 +0,0 @@
-// components/ui/index.tsx
-
-export { default as Button } from './button';
-export { default as Input } from './input';
-export { default as Textarea } from './textarea';
-// Add other component exports here as needed
